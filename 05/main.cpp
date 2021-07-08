1862f1c380835bfc627004fbad935e280f8e7aae
1230V2 <lihongtu12138@outlook.com>
2021-07-08 13:00:37 +0800

    重载()

--- 05/main.cpp
+++ 05/main.cpp
@@ -2,18 +2,38 @@
 
 using namespace std;
 
-int a[] = {1, 2, 3, 4};
+//int a[] = {1, 2, 3, 4};
+//
+//void func1(int b[]){
+//    b[0] = 10;
+//}
+//
+//void func2(int b[]){
+//    cout << b[0] << endl;
+//}
 
-void func1(int b[]){
-    b[0] = 10;
-}
+//int main() {
+//    func1(a);
+//    func2(a);
+//    return 0;
+//}
 
-void func2(int b[]){
-    cout << b[0] << endl;
-}
+class MyCompare{
+public:
+    bool operator()(int a, int b){
+        return a > b;
+    }
+};
+
 
 int main() {
-    func1(a);
-    func2(a);
+    // 通过对象调用
+    MyCompare myCompare;
+    bool res = myCompare(2, 1);
+    cout << res << endl;    // 1
+
+    // 类名两个括号直接调用
+    res = MyCompare()(1, 2);
+    cout << res << endl;    // 0
     return 0;
 }
