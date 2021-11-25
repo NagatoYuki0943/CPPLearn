fa1bed33e150e8b43bae52d04ef1e94e08842f9d
P775TM1 <lihongtu12138@outlook.com>
2021-11-25 21:44:08 +0800

    删除多余空格

--- 01/study01/13zi_fu_chuan.cpp
+++ 01/study01/13zi_fu_chuan.cpp
@@ -15,7 +15,7 @@ int main() {
 }
 /**
  * C类型字符串
-	char类型字符串 必须要以空字符结尾才是字符串 
+	char类型字符串 必须要以空字符结尾才是字符串
 	char dog[8] = {'b', 'e', 'a', 'u', 'x', ' ', 'I', 'I' };     只是一个数组,不是字符串
 	char cat[8] = {'f', 'a', 't', 'e', 's', 's', 'a', '\0' };      是一个字符串
 	另一种简单写法 用括号括起来的字符串隐式的包括结尾的空字符,不用显示的包括它
@@ -27,7 +27,7 @@ int main() {
 	但是 "S" 不是字符常量,它表示的是两个字符(字符S和\0)组成的字符串.更糟的是,"S"表示的实际是字符串所在的内存地址.因此下面的语句试图将一个内存地址赋值给ch
 	char ch = "s";
 	由于地址在C++中是一种独立的类型,因此C++编译器不允许这种不合理的做法
-	
+
 	标准头文件 cstring提供了很多与字符串相关的函数的声明
 	strlen() 函数返回字符串的长度,只能用于char数组,不能用于string类型,strcpy(),strcat().cin.getline(),cin.get()同理
 	strcpy(str1, str2 ); 将str2的值赋值给str1
@@ -39,30 +39,30 @@ int main() {
 	string str2="asdfaed";
 	int len1=strlen(str1);
 	//int len2=strlen(str2);   //strlen函数不能检查string类型的字符串的长度
-	    
+
 	strcpy(str1,str3);
 	//strcpy(str1,str2);       //strcpy只能用于char类型,不能用于string类型
 	strcat(str1,str3);
-	 //strcat(str1,str2);       //strcat只能用于char类型,不能用于string类型
-	
+	//strcat(str1,str2);       //strcat只能用于char类型,不能用于string类型
+
 	注意 : cin 输入字符串遇到空格,回车会结束,所以不能输入带有空格的字符串
 	要使用 cin.getline() 和 cin.get()输入带有空格的字符;
 	cin是iostream的一个对象,getline和get是类内的方法
 	cin.getline(字符串名字,长度)   cin.get(字符串名字,长度)
 	getline()每次读取一行,它通过换行符结束,getline()将换行符丢弃,将换行符转换为空格 ,get()会将换行符保留在输入队列中,下一个输入的就是换行
-	
+
 	对于get()会保留空格,可以使用下面的方法接收
 	cin.get(name,20);
 	cin.get();
 	cin.get(dessert,50);
 	cin.get();
-	
+
 	或者
 	cin.get(name,20).get();
 	cin.get(dessert,50).get();
-	
+
 	混合输入数字和字符串会出现问题
-	
+
 	int year;
 	cin >> year;
 	cin.get();   //or cin.get(ch)    如果没有这一行,输入year之后不会输入address了,下面一行和上面两行同理
@@ -84,10 +84,9 @@ C++11初始化
 	如果开始不给任何值,自动设置为0
 	char str1[10] {1,2,3};
 	string str2[5] {1,2,5,6};
-	
 
 	char ch[20];
 	string str;
 	cin.getline(ch,20);   //类内方法,第一个参数的目标数组,第二个参数是长度,getline不能用于string类型
 getline(cin,str);         //这一行没有 .  说明不是类中的方法  它把cin作为参数,指出到哪里去查找输入
- */ 
+ */
--- 01/study01/42array.cpp
+++ 01/study01/42array.cpp
@@ -24,14 +24,14 @@ int main() {
 /**
  * c++和C#的不同之处
  * C/C++数组声明:  类型 数组名[]={};              int arr[]={};
- *  
+ *
  * 指针分为  常量的指针:const int * p     指针是常量:int * const p;   双:const int * const p;
- * C++中new是在堆区创建数据, int * p = new int(10);  删除使用delete 
- * 在堆区中创建数组   int * p = new int[10];         删除使用delete[] 
- * 
+ * C++中new是在堆区创建数据, int * p = new int(10);  删除使用delete
+ * 在堆区中创建数组   int * p = new int[10];         删除使用delete[]
+ *
  * C#数组声明: 类型[] 数组名 = new 类型名[]{}     int[] ints=new int[6]{1,2,3,4,5,6};
- * 
- *  在C++11中 初始化可以省略 "="  
+ *
+ *  在C++11中 初始化可以省略 "="
  *	int i[5] {1, 5, 2, 3, 6}
- *  float f[] {1.5, 25, 0.552, 2.55}    
+ *  float f[] {1.5, 25, 0.552, 2.55}
  */
