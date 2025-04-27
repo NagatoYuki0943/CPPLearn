17cc381f35202b1e08ed07653d73c6ce7043c742
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-04-27 14:08:52 +0800

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -6,24 +6,28 @@ int main() {
 	std::time_t t1 = std::time(0); // 获取当前时间戳（秒）
 	std::cout << t1 << " seconds since 01-Jan-1970" << std::endl;
 	
-	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	// 可以使用 int 类型保存
+	int t2 = std::time(0); // 获取当前时间戳（秒）
+	std::cout << t2 << " seconds since 01-Jan-1970" << std::endl;
+	
+	std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();
 
-	auto ms1 = std::chrono::duration_cast<std::chrono::seconds>(t2.time_since_epoch());
+	auto ms1 = std::chrono::duration_cast<std::chrono::seconds>(t3.time_since_epoch());
 	std::cout << ms1.count() << " seconds since 01-Jan-1970"  << std::endl; // 输毫秒级时间戳
 	
-	auto ms2 = std::chrono::duration_cast<std::chrono::milliseconds>(t2.time_since_epoch());
+	auto ms2 = std::chrono::duration_cast<std::chrono::milliseconds>(t3.time_since_epoch());
 	std::cout << ms2.count() << " milliseconds since 01-Jan-1970"  << std::endl; // 输出毫秒级时间戳
 
-	auto ms3 = std::chrono::duration_cast<std::chrono::microseconds>(t2.time_since_epoch());
+	auto ms3 = std::chrono::duration_cast<std::chrono::microseconds>(t3.time_since_epoch());
 	std::cout << ms3.count() << " microseconds since 01-Jan-1970"  << std::endl; // 输出微秒级时间戳
 	
-	auto ms4 = std::chrono::duration_cast<std::chrono::nanoseconds>(t2.time_since_epoch());
+	auto ms4 = std::chrono::duration_cast<std::chrono::nanoseconds>(t3.time_since_epoch());
 	std::cout << ms4.count() << " nanoseconds since 01-Jan-1970"  << std::endl; // 输出纳秒级时间戳
 	
-	auto ms5 = std::chrono::duration_cast<std::chrono::minutes>(t2.time_since_epoch());
+	auto ms5 = std::chrono::duration_cast<std::chrono::minutes>(t3.time_since_epoch());
 	std::cout << ms5.count() << " minutes since 01-Jan-1970"  << std::endl; // 输出分钟级时间戳
 	
-	auto ms6 = std::chrono::duration_cast<std::chrono::hours>(t2.time_since_epoch());
+	auto ms6 = std::chrono::duration_cast<std::chrono::hours>(t3.time_since_epoch());
 	std::cout << ms6.count() << " hours since 01-Jan-1970"  << std::endl; // 输出小时级时间戳
 
 	return 0;
